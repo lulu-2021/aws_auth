41a359196308a22202d9b76538a789a5a7c8007a
Lolo <482086+lulu-2021@users.noreply.github.com>
2025-10-07 15:00:42 +1100

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -3,12 +3,11 @@ defmodule AWSAuth.Mixfile do
 
   def project do
     [app: :aws_auth,
-     version: "0.7.3",
-     elixir: "~> 1.3",
+     version: "0.8.1",
+     elixir: "~> 1.18.4",
      description: description(),
      package: package(),
      deps: deps(),
-     test_coverage: [tool: ExCoveralls],
      preferred_cli_env: [coveralls: :test]
     ]
   end
@@ -19,10 +18,9 @@ defmodule AWSAuth.Mixfile do
 
   defp deps do
     [
-      {:earmark, "~> 1.2.3", only: :dev },
+      {:earmark, "~> 1.4.48", only: :dev },
       {:ex_doc, "~> 0.16", only: :dev },
-      {:excoveralls, "~> 0.4", only: :test},
-      {:credo, "~> 0.8.6", only: [:dev, :test]}
+      {:credo, "~> 1.7.12", only: [:dev, :test]}
     ]
   end
 
